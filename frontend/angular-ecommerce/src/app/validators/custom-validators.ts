f4790872053bffcb63d05332d15c3642883f97a2
Hazem Samir <hazemsabdallah@gmail.com>
2024-04-09 00:37:57 +0200

    adding form control validations

---
+++ frontend/angular-ecommerce/src/app/validators/custom-validators.ts
@@ -0,0 +1,13 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+export class CustomValidators {
+
+    static notOnlyWhitespace(formControl: FormControl): ValidationErrors | null {
+        if (formControl.value != null && formControl.value.trim().length === 0) {
+            return {'notOnlyWhitespace': true}
+        }
+        else {
+            return null;
+        }
+    }
+}
