c598993579d8f261b7d23c9ca9549a24bc951fe1
Hazem Samir <hazemsabdallah@gmail.com>
2024-04-14 19:59:02 +0200

    adding support to save an order in checkout

---
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,60 @@
+package com.luv2code.ecommerce.service;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with items
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(order::addItem);
+
+        // populate order with billing and shipping addresses
+        order.setShippingAddress(purchase.getShippingAddress());
+        order.setBillingAddress(purchase.getBillingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.addOrder(order);
+
+        // save to db
+        customerRepository.save(customer);
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // random uuid
+        return UUID.randomUUID().toString();
+    }
+}
